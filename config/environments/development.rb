b08a1adb16a1e1ad4f361ca1caf4f3fd3aed5731
RyanGao67 <tiangao0611@gmail.com>
2023-02-03 11:57:49 -0500

    a

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      'tiangao0611@gmail.com',
-    password:       'psvtmovxibihmveg',
+    password:       '',
     domain:         'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
